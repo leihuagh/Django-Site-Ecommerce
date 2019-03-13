21146ecebb2374458c83d057048eb34019437777
Lei Hua <lhua@example.com>
2019-03-13 16:29:35 -0400

    two more routes

--- core/views.py
+++ core/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+def about(request):
+    return render(request, 'index.html', {})
+
+
+def contact(request):
+    return render(request, 'index.html', {})
