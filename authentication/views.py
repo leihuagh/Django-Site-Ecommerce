8f2cc3deaf00afc0c0beaea812eadee551a311bb
Lei Hua <lhua@example.com>
2019-03-13 22:57:45 -0400

    app authentication

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+def login_view(request):
+    context = {
+        "title": "Login",
+        "content": "Login Form",
+    }
+    return render(request, 'login.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
