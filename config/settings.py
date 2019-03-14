53080f301c18df4fda8a80b726331ce8e3c40060
Lei Hua <lhua@example.com>
2019-03-13 22:37:36 -0400

    app authentication

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
+    'authentication.apps.AuthenticationConfig',
 ]
 
 MIDDLEWARE = [
